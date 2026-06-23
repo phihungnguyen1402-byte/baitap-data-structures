e4d5f96255bbfcb153e9fac18631e78471e9be74
Hung <phihungnguyen1402@gmail.com>
2026-06-23 10:42:17 +0700

    fix

--- ringbuffer.c
+++ ringbuffer.c
@@ -31,3 +31,29 @@ bool RingBuffer_Read(RingBuffer_t *rb, uint8_t *data) {
     rb->isFull = false;
     return true; 
 }
+
+// Hàm đọc liên tục cho đến khi gặp stopChar hoặc kho rỗng
+bool RingBuffer_ReadUntil(RingBuffer_t *rb, uint8_t *outBuffer, uint8_t stopChar) {
+    int i = 0;
+    uint8_t data;
+
+    // Vòng lặp: Chừng nào kho còn đồ để lấy (gọi hàm Read thành công)
+    while (RingBuffer_Read(rb, &data) == true) {
+        
+        // BƯỚC 1: Bỏ byte 'data' vừa gắp được vào mảng 'outBuffer' tại vị trí 'i'
+        outBuffer[i] = data;
+        
+        // BƯỚC 2: Kiểm tra xem 'data' có trúng với 'stopChar' không?
+        if (data == stopChar) {
+            outBuffer[i + 1] = '\0'; // Chốt đuôi \0 để tạo thành chuỗi String chuẩn C
+            return true;             // Báo hiệu đã gắp được một lệnh hoàn chỉnh
+        }
+
+        // BƯỚC 3: Tăng 'i' lên 1 bước để chuẩn bị cất byte tiếp theo
+        i++;
+    }
+
+    // Đọc cạn kiệt kho rồi mà vẫn không thấy stopChar
+    outBuffer[i] = '\0'; // Vẫn phải chốt đuôi mảng cho an toàn
+    return false;        // Báo hiệu là chưa thu thập đủ một lệnh hoàn chỉnh
+}
